bb598b80a1808f5b23a08f5fe891b178117b4985
Yuliya Byshko <Yuliyalevkovets1303@gmail.com>
2022-04-27 11:30:50 +0200

    Made setUp method as static

--- src/test/java/com/yuliya1303/tests/GitIssuesTest.java
+++ src/test/java/com/yuliya1303/tests/GitIssuesTest.java
@@ -1,13 +1,11 @@
 package com.yuliya1303.tests;
 
-import com.codeborne.selenide.logevents.SelenideLogger;
 import com.yuliya1303.pages.GitIssuesPage;
 import io.qameta.allure.*;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class GitIssuesTest {
+public class GitIssuesTest extends TestBase {
     String REPOSITORY = "Yuliya1303/homework5---JUnit";
 
     @Test
@@ -17,7 +15,6 @@ public class GitIssuesTest {
     @Story("Issue tab is displayed by default")
     @DisplayName("Issue tab is displayed by default test")
     void isIssueTabExists() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
         GitIssuesPage steps = new GitIssuesPage();
 
         steps.openMainPage();
---
+++ src/test/java/com/yuliya1303/tests/TestBase.java
@@ -0,0 +1,34 @@
+package com.yuliya1303.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.yuliya1303.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase {
+
+    @Test
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.browser = "Chrome";
+        Configuration.browserSize = "1920*1200";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    void addAttachments() {
+        Attach.screenshotAs("Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
