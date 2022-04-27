fec92323cbb8f28b0d8a9405e55c52027b322eb8
Yuliya Byshko <Yuliyalevkovets1303@gmail.com>
2022-04-27 11:47:56 +0200

    Added Tags

--- src/test/java/com/yuliya1303/tests/GitIssuesTest.java
+++ src/test/java/com/yuliya1303/tests/GitIssuesTest.java
@@ -3,17 +3,19 @@ package com.yuliya1303.tests;
 import com.yuliya1303.pages.GitIssuesPage;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class GitIssuesTest extends TestBase {
     String REPOSITORY = "Yuliya1303/homework5---JUnit";
 
     @Test
+    @DisplayName("Issue tab is displayed by default test")
     @Owner("yuliyabyshko")
     @Severity(SeverityLevel.BLOCKER)
     @Feature("Repositories - Issues")
     @Story("Issue tab is displayed by default")
-    @DisplayName("Issue tab is displayed by default test")
+    @Tag("Smoke")
     void isIssueTabExists() {
         GitIssuesPage steps = new GitIssuesPage();
 
